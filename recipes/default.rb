5feb1616be45703c5e8a32fa737d5ef28b3c8147
aamirahmad <aamir.ahamd170@gmail.com>
2017-04-01 09:15:41 +0530

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
-#
-# Cookbook:: edureka-nginx
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'nginx' do
+	action :install
+end
+	
+service 'nginx' do
+	action [:enable, :start]
+end
+
+cookbook_file "/var/www/html/index.nginx-debian.html"
+	source 'index.html'
+	mode '0644'
+end
